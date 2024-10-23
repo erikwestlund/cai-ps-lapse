12cba4208b764fc6d6de8756a7593585db561e12
Erik Westlund <ewestlu1@jh.edu>
2024-10-23 11:56:21 -0400

    wip

---
+++ settings.R
@@ -0,0 +1,12 @@
+data_last_updated <- as.Date('2023-08-29')
+
+# Working directly on SAFE Desktop:
+s_root <- "S:/SDoHinDRWestlundE/"
+
+# Workign on Crunchr:
+#s_root <- "~/workspace/SDoHinDRWestlundE" 
+
+# You may need to alter the CSV file for your source data.
+source_file_path <- paste0(s_root, "/df_update_lapses_to_EW.csv")
+
+
